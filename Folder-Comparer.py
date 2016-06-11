bcdb2e03c0334f436bed4b4cfd1caff556d99f2d
Oliver Thunæs <Felolis30@gmail.com>
2016-06-12 00:53:56 +0200

    Now knows the difference in images between two folders

--- Folder-Comparer.py
+++ Folder-Comparer.py
@@ -1,16 +1,32 @@
-import glob
+import os
+from shutil import copyfile
 
-original_folder = glob.glob('C:/Users/oliverat/Dropbox/Shoit/Smug/*')
+original_folder = os.listdir('C:/Users/Oliver/Dropbox/Shoit/Smug')
 
-new_folder = glob.glob('smug/*')
+new_folder = os.listdir('images')
 
-print(original_folder)
 
-def sync_folder(original_folder, new_folder):
-    pass
+def find_difference(source_folder, dest_folder):
+    """
+    :param source_folder: A folder of images
+    :param dest_folder: A folder of images
+    :return: All the images that are in source_folder but not in dest_folder
+    """
+    difference = []
+    for img in source_folder:
+        if img not in dest_folder:
+            difference = difference + [img]
+    sync_files(difference, 'images/')
+
+
+def sync_files(difference, destination):
+    for i in range(len(difference)):
+        copyfile(difference[i], destination)
+
 
 def main():
-    pass
+    find_difference(original_folder, new_folder)
+
 
 if __name__ == "__main__":
     main()
