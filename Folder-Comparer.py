e3837720c5db8cbab295ef24b1fab6a67b6217e6
Oliver Andreas Thunæs <oliverat@ostfoldfk.no>
2016-06-11 17:25:38 +0200

    Added reading of folder

--- Folder-Comparer.py
+++ Folder-Comparer.py
@@ -1,2 +1,16 @@
 import glob
 
+original_folder = glob.glob('C:/Users/oliverat/Dropbox/Shoit/Smug/*')
+
+new_folder = glob.glob('smug/*')
+
+print(original_folder)
+
+def sync_folder(original_folder, new_folder):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
